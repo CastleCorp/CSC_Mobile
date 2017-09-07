098912b36fe80b1e5b0d7baff8cf1d903f34b28b
Parker Thomas <pthomas1997@gmail.com>
2017-09-07 15:02:40 -0400

    finished Pascal's Triangle

--- PascalsTriangle.playground/Contents.swift
+++ PascalsTriangle.playground/Contents.swift
@@ -9,17 +9,31 @@
 import Foundation
 
 func factorial(number n: Int) -> Int {
-    var sum = 1
-    for i in 1...n {
-        sum = sum * i
+    if n <= 1 {
+        return 1
     }
-    return sum
+    return n * factorial(number: n-1)
 }
 
-func choose(nNumber n: Int, kNumber k: Int) {
-    // compute (n)     n!
-    //         (k) = --------
-    //               k!(n-k)!
+func choose(row n: Int, cell k: Int) -> Int {
+    return (factorial(number: n) / (factorial(number: k) * factorial(number: n - k)))
 }
 
-print(factorial(number: 5))
+// How many rows to generate
+let rows = 5
+
+// r stands for row, c stands for c
+for r in 0..<rows {
+    var currentRow = [Int]()
+    var rowString = ""
+    
+    for c in 0...r {
+        currentRow.append(choose(row: r, cell: c))
+    }
+    
+    for i in 0..<currentRow.count {
+        rowString = rowString + " " + String(currentRow[i])
+    }
+    
+    print(rowString)
+}
