77cda56fe4fcee2b143fefc23672410ce3d548e0
Parker Thomas <pthomas1997@gmail.com>
2017-10-06 18:11:30 -0400

    Finish Match Game

--- Apps/SixteenCards/SixteenCards/ViewController.swift
+++ Apps/SixteenCards/SixteenCards/ViewController.swift
@@ -143,13 +143,6 @@ class ViewController: UIViewController {
 //        }
         
         
-<<<<<<< HEAD
-=======
-        if(!matchGameModel.matchesRemaining()) {
-        
-        }
->>>>>>> aedd8f27bfb53af5b8a9d98c935efd012c961bb3
-        
         if(matchGameModel.getCardImage(which) == #imageLiteral(resourceName: "back_of_card") /*&& matchGameModel.matchesRemaining()*/) {
             
             matchGameModel.flipCard(which)
