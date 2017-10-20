2051b24691657e0e44725a0389fc74af275120ed
Parker Thomas <pthomas7@duke303-25.elon.edu>
2017-10-20 12:08:44 -0400

    Multitouch

---
+++ Multitouch/Multitouch/GameViewController.swift
@@ -0,0 +1,45 @@
+//
+//  GameViewController.swift
+//  Multitouch
+//
+//  Created by Parker Thomas on 10/20/17.
+//  Copyright © 2017 Parker Thomas. All rights reserved.
+//
+
+import UIKit
+import SpriteKit
+import GameplayKit
+
+class GameViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        let scene = GameScene(size: view.bounds.size)
+        let skView = view as! SKView
+        scene.scaleMode = .resizeFill
+        
+        skView.presentScene(scene)
+    }
+
+    override var shouldAutorotate: Bool {
+        return true
+    }
+
+    override var supportedInterfaceOrientations: UIInterfaceOrientationMask {
+        if UIDevice.current.userInterfaceIdiom == .phone {
+            return .allButUpsideDown
+        } else {
+            return .all
+        }
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Release any cached data, images, etc that aren't in use.
+    }
+
+    override var prefersStatusBarHidden: Bool {
+        return true
+    }
+}
